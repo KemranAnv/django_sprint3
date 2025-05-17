049e284e8985505f9467f81e182cbe2f6bf14e03
Annekov Kemran <kemran.anv@gmail.com>
2025-05-17 04:07:00 +0300

    Added models(Post, Category, Location) to blog.models(without поле).

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,3 +1,25 @@
-# from django.db import models
+"""Создание моделей."""
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model
+
 
 # Create your models here.
+class Post(models.Model):
+    """Публикация."""
+
+    pass
+
+
+class Category(models.Model):
+    """Тематическая категория."""
+
+    pass
+
+
+class Location(models.Model):
+    """Географическая метка."""
+
+    pass
