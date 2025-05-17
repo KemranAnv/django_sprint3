7d516bfb3587206e7076f4badf8153ed2082b84c
Annekov Kemran <kemran.anv@gmail.com>
2025-05-17 05:06:34 +0300

    Fixed verbose_name, get_user_model in blog.models .

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -3,29 +3,29 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 
 # Create your models here.
 class Category(models.Model):
     """Тематическая категория."""
 
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание')
+    title = models.CharField('Заголовок', max_length=256)
+    description = models.TextField('Описание')
     slug = models.SlugField(
+        'Идентификатор',
         unique=True,
         help_text=(
             'Идентификатор страницы для URL; '
             'разрешены символы латиницы, цифры, дефис и подчёркивание.'
         ),
-        verbose_name='Идентификатор'
-        )
-    is_published = models.BooleanField(
-        default=True,
-        help_text='Снимите галочку, чтобы скрыть публикацию.',
-        verbose_name='Опубликовано'
     )
-    created_at = models.DateTimeField(verbose_name='Добавлено')
+    is_published = models.BooleanField(
+        'Опубликовано',
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField('Добавлено')
 
     class Meta:
         """Мета файлы для Category."""
@@ -37,15 +37,15 @@ class Category(models.Model):
 class Location(models.Model):
     """Географическая метка."""
 
-    name = models.CharField(max_length=256, verbose_name='Название места')
+    name = models.CharField('Название места', max_length=256)
     is_published = models.BooleanField(
+        'Опубликовано',
         default=True,
-        help_text='Снимите галочку, чтобы скрыть публикацию.',
-        verbose_name='Опубликовано'
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
     created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Добавлено'
+        'Добавлено',
+        auto_now_add=True
     )
 
     class Meta:
@@ -58,41 +58,41 @@ class Location(models.Model):
 class Post(models.Model):
     """Публикация."""
 
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    text = models.TextField(verbose_name='Текст')
+    title = models.CharField('Заголовок', max_length=256, )
+    text = models.TextField('Текст')
     pub_date = models.DateTimeField(
+        'Дата и время публикации',
         help_text=(
             'Если установить дату и время в будущем '
             '— можно делать отложенные публикации.'
-        ),
-        verbose_name='Дата и время публикации'
+        )
     )
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор публикации'
+        verbose_name='Автор публикации',
+        on_delete=models.CASCADE
     )
     location = models.ForeignKey(
         Location,
+        verbose_name='Местоположение',
         on_delete=models.SET_NULL,
         null=True,
-        blank=True,
-        verbose_name='Местоположение'
+        blank=True
     )
     category = models.ForeignKey(
         Category,
+        verbose_name='Категория',
         on_delete=models.SET_NULL,
-        null=True,
-        verbose_name='Категория'
+        null=True
     )
     is_published = models.BooleanField(
+        'Опубликовано',
         default=True,
-        help_text='Снимите галочку, чтобы скрыть публикацию.',
-        verbose_name='Опубликовано'
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
     created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Добавлено'
+        'Добавлено',
+        auto_now_add=True
     )
 
     class Meta:
