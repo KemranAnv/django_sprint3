429f96eb2772101f3a454795a584c38fc5ba8efb
Annekov Kemran <kemran.anv@gmail.com>
2025-05-17 04:43:07 +0300

    Added verbose_name in blog.models.

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -10,41 +10,76 @@ User = get_user_model
 class Category(models.Model):
     """Тематическая категория."""
 
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField()
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(unique=True, verbose_name='Идентификатор')
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
+    created_at = models.DateTimeField(verbose_name='Добавлено')
+
+    class Meta:
+        """Мета файлы для Category."""
+
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 
 class Location(models.Model):
     """Географическая метка."""
 
-    name = models.CharField(max_length=256)
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=256, verbose_name='Название места')
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано')
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
+
+    class Meta:
+        """Мета файлы для Location."""
+
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
 
 
 class Post(models.Model):
     """Публикация."""
 
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(verbose_name='Дата и время публикации')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
         null=True,
-        blank=True
+        blank=True,
+        verbose_name='Местоположение'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
+        verbose_name='Категория'
     )
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
+
+    class Meta:
+        """Мета файлы для Post."""
+
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
