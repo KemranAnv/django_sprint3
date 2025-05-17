c3c06ad4159a6978c05b64dd471f7e095dcb522c
Annekov Kemran <kemran.anv@gmail.com>
2025-05-17 04:51:30 +0300

    Added hints to fields in blog.models .

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -12,9 +12,17 @@ class Category(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(unique=True, verbose_name='Идентификатор')
+    slug = models.SlugField(
+        unique=True,
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        ),
+        verbose_name='Идентификатор'
+        )
     is_published = models.BooleanField(
         default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.',
         verbose_name='Опубликовано'
     )
     created_at = models.DateTimeField(verbose_name='Добавлено')
@@ -32,7 +40,9 @@ class Location(models.Model):
     name = models.CharField(max_length=256, verbose_name='Название места')
     is_published = models.BooleanField(
         default=True,
-        verbose_name='Опубликовано')
+        help_text='Снимите галочку, чтобы скрыть публикацию.',
+        verbose_name='Опубликовано'
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Добавлено'
@@ -50,7 +60,13 @@ class Post(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(verbose_name='Дата и время публикации')
+    pub_date = models.DateTimeField(
+        help_text=(
+            'Если установить дату и время в будущем '
+            '— можно делать отложенные публикации.'
+        ),
+        verbose_name='Дата и время публикации'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -71,6 +87,7 @@ class Post(models.Model):
     )
     is_published = models.BooleanField(
         default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.',
         verbose_name='Опубликовано'
     )
     created_at = models.DateTimeField(
