9b215f489738505110d6de428251a216321a79e3
Annekov Kemran <kemran.anv@gmail.com>
2025-05-18 14:45:23 +0300

    Upgrated blog.admin.

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,9 +1,38 @@
 """Admin zone of blog."""
 
 from django.contrib import admin
+from .models import Post, Category, Location
 
-from .models import Category, Location, Post
 
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """PostAdmin."""
+
+    list_display = ('title', 'pub_date', 'author', 'location',
+                    'is_published', 'created_at', 'category')
+    list_editable = ('is_published', 'category')
+    search_fields = ('title',)
+    list_filter = ('category', 'is_published')
+    list_display_links = ('title',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """CategoryAdmin."""
+
+    list_display = ('title', 'created_at', 'is_published')
+    list_editable = ('is_published',)
+    search_fields = ('title',)
+    list_filter = ('is_published',)
+    list_display_links = ('title',)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    """LocationAdmin."""
+
+    list_display = ('name', 'is_published', 'created_at')
+    list_editable = ('is_published',)
+    search_fields = ('name',)
+    list_filter = ('is_published',)
+    list_display_links = ('name',)
